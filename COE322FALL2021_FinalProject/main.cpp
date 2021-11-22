cfa89bb22efa580719cbac5672d963d14d72c866
Aleninja747 <41636380+Aleninja747@users.noreply.github.com>
2021-11-22 13:01:43 -0600

    Initial Commit

---
+++ COE322FALL2021_FinalProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  COE322FALL2021_FinalProject
+//
+//  Created by Jorge Alejandro Ricaurte on 11/22/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
