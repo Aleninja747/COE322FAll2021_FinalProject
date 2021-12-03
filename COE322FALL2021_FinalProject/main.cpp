5d3ff941b86343268d04a33bf925568d73e11885
Aleninja747 <41636380+Aleninja747@users.noreply.github.com>
2021-12-03 13:03:40 -0600

    Implemented Voter Class

--- COE322FALL2021_FinalProject/main.cpp
+++ COE322FALL2021_FinalProject/main.cpp
@@ -8,15 +8,22 @@
 #include <iostream>
 #include <vector>
 
+using std::vector;
+using std::cout;
+
 class Voter{
 private:
     int voter_id{0}, affiliation{0};
 public:
     Voter(int id, int afill) : voter_id(id), affiliation(afill){};
+    Voter(vector<int> input_vec): voter_id(input_vec.at(0)), affiliation(input_vec.at(1)){};
+    void print(){
+        cout<<"Voter ID:"<<voter_id<<" Affiliation:"<<affiliation<<std::endl;
+    };
 };
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Voter test(0,1);
+    test.print();
     return 0;
 }
