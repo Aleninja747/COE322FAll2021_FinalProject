f151d2282b4adc909dfae636e76fbb659fbbc010
Aleninja747 <41636380+Aleninja747@users.noreply.github.com>
2021-12-03 13:45:16 -0600

    Implemented most of District Class

--- COE322FALL2021_FinalProject/main.cpp
+++ COE322FALL2021_FinalProject/main.cpp
@@ -7,8 +7,10 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 using std::vector;
+using std::string;
 using std::cout;
 
 class Voter{
@@ -20,10 +22,62 @@ public:
     void print(){
         cout<<"Voter ID:"<<voter_id<<" Affiliation:"<<affiliation<<std::endl;
     };
+    int id(){
+        return voter_id;
+    };
+    int get_affiliation(){
+        return affiliation;
+    };
+};
+
+class District{
+private:
+    vector<Voter> voter_vec;
+public:
+    District(Voter input_voter){voter_vec.push_back(input_voter);};
+    District(vector<Voter> input_vec): voter_vec(input_vec){};
+    District(string input_str)
+    {
+        for (int i=0; i<input_str.size(); i++) {
+            if (input_str.at(i)=='-') {
+                Voter temp_voter(i,-1);
+                voter_vec.push_back(temp_voter);
+            }
+            else if (input_str.at(i)=='+'){
+                Voter temp_voter(i,1);
+                voter_vec.push_back(temp_voter);
+            }
+            else{
+                Voter temp_voter(i,0);
+                voter_vec.push_back(temp_voter);
+            }
+        }
+    };
+    int lean(){
+        int counter_A=0,counter_B=0;
+        for (int i =0; i<voter_vec.size(); i++) {
+            if (voter_vec.at(i).get_affiliation()==-1) {
+                counter_A++;
+            }
+            else if (voter_vec.at(i).get_affiliation()==1){
+                counter_B++;
+            }
+        }
+        if (counter_B>counter_A) {
+            return 1;
+        }
+        else if (counter_A>counter_B){
+            return -1;
+        }
+        return 0;
+    }
 };
 
 int main(int argc, const char * argv[]) {
-    Voter test(0,1);
+    Voter test(1,1);
     test.print();
+    string str("hello"),str2("h");
+    
+    cout<<(str.at(0)=='g')<<std::endl;
     return 0;
 }
