39540bcb000d63e78510208b00d8c2a8defb6a55
Aleninja747 <41636380+Aleninja747@users.noreply.github.com>
2021-12-03 12:44:54 -0600

    Test to commit

--- COE322FALL2021_FinalProject/main.cpp
+++ COE322FALL2021_FinalProject/main.cpp
@@ -6,8 +6,13 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
+class Voter{
+private:
+};
+
+int main() {
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
