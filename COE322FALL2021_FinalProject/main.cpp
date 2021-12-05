c4101fddbe4ab3437f829d2c82d087ed076828db
Aleninja747 <41636380+Aleninja747@users.noreply.github.com>
2021-12-04 18:46:23 -0600

    Implemented sorting algorithm and Route class.

--- COE322FALL2021_FinalProject/main.cpp
+++ COE322FALL2021_FinalProject/main.cpp
@@ -20,6 +20,7 @@ class Address{
 private:
     double i_position,j_position;
 public:
+    Address():i_position(0), j_position(0){};
     Address(double i, double j): i_position(i), j_position(j){};
     double get_i_position(){
         return i_position;
@@ -42,12 +43,20 @@ public:
 };
 
 class Address_list{
-private:
+protected:
     vector<Address> address_vec;
 public:
     Address_list(){};
     void add_address(Address input_address){
-        address_vec.push_back(input_address);
+        bool is_unique=true;
+        for (int i=0; i<address_vec.size(); i++) {
+            if (address_vec.at(i).equal(input_address)) {
+                is_unique=false;
+            }
+        }
+        if (is_unique) {
+            address_vec.push_back(input_address);
+        }
     };
     double length(bool manhattan_dist=false){
         double temp_length=0;
@@ -66,6 +75,39 @@ public:
             }
         }
         return address_vec.at(closest_index);
+    }
+    vector<Address> get_address_vec(){
+        return address_vec;
+    }
+};
+
+class Route: public Address_list{
+private:
+    Address depot;
+public:
+    Route(Address_list input_address_list){
+        this->address_vec = input_address_list.get_address_vec();
+        depot=address_vec.at(0);
+    };
+    void greedy_route(){
+        vector<Address> remaining_address = this->address_vec;
+        vector<Address> route_vec;
+        route_vec.push_back(depot);
+        remaining_address.erase(remaining_address.begin());
+        while (remaining_address.size()>0) {
+            Address current_address = route_vec.back();
+            double min_distance = std::numeric_limits<int>::max();
+            int closest_index=0;
+            for (int i=0; i<remaining_address.size(); i++) {
+                if (remaining_address.at(i).distance(current_address)<min_distance) {
+                    min_distance =remaining_address.at(i).distance(current_address);
+                    closest_index=i;
+                }
+            }
+            route_vec.push_back(remaining_address.at(closest_index));
+            remaining_address.erase(remaining_address.begin()+(closest_index-1));
+        }
+        this->address_vec = route_vec;
     }
 };
 
