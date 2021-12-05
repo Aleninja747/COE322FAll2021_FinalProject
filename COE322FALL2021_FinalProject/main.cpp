2d865bcd09b09db5e7c4c3e2317d11321cd2fb55
Aleninja747 <41636380+Aleninja747@users.noreply.github.com>
2021-12-04 18:08:25 -0600

    First Commit: implemented Address class and partial Address_list class

--- COE322FALL2021_FinalProject/main.cpp
+++ COE322FALL2021_FinalProject/main.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <string>
 #include <math.h>
+#include <limits>
 
 
 using std::vector;
@@ -32,8 +33,47 @@ public:
         }
         return sqrt(pow(other_address.get_i_position()-i_position,2)+pow(other_address.get_j_position()-j_position,2));
     }
+    bool equal(Address input_address){
+        if (input_address.get_i_position()==i_position && input_address.get_j_position()==j_position){
+            return true;
+        }
+        return false;
+    };
+};
+
+class Address_list{
+private:
+    vector<Address> address_vec;
+public:
+    Address_list(){};
+    void add_address(Address input_address){
+        address_vec.push_back(input_address);
+    };
+    double length(bool manhattan_dist=false){
+        double temp_length=0;
+        for (int i=1; i<address_vec.size(); i++) {
+            temp_length+=address_vec.at(i-1).distance(address_vec.at(i),manhattan_dist);
+        }
+        return temp_length;
+    }
+    Address index_closest_to(Address input_address){
+        double min_distance = std::numeric_limits<int>::max();
+        int closest_index=0;
+        for (int i=0; i<address_vec.size(); i++) {
+            if (address_vec.at(i).distance(input_address)<min_distance) {
+                min_distance =address_vec.at(i).distance(input_address);
+                closest_index=i;
+            }
+        }
+        return address_vec.at(closest_index);
+    }
 };
 
 int main() {
-
+    Address address_1(1,1), address_2(2,2), address_3(3,3);
+    Address_list list;
+    list.add_address(address_1);
+    list.add_address(address_2);
+    list.add_address(address_3);
+    cout<< list.length();
 }
